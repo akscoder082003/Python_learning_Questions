69b391d3c4fb51cec6cb7d81186f05c923e713aa
akscoder082003 <ashishk.singh1008@gmail.com>
2023-10-11 19:38:13 +0530

    commit

---
+++ 9/code.py
@@ -0,0 +1,7 @@
+# Write a python program that accepts an integer (n) and computes the value of n+nn+nnn. 
+
+a = int(input("Input an integer : "))
+n1 = int("%s"%a)
+n2 = int("%s%s"%(a,a))
+n3 = int("%s%s%s"%(a,a,a))
+print(n1+n2+n3)
