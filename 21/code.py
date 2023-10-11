c410e56fd1fd506e06aed2251d4900933c473ec5
akscoder082003 <ashishk.singh1008@gmail.com>
2023-10-11 20:29:27 +0530

    commit

---
+++ 21/code.py
@@ -0,0 +1,13 @@
+# Write a Python program to count the number 4 in a given list.
+
+def list_count_4(nums):
+    count = 0
+    for num in nums:
+        if num == 4:
+            count = count + 1
+
+    return count
+
+
+print(list_count_4([1, 4, 6, 7, 4]))
+print(list_count_4([1, 4, 6, 4, 7, 4]))
